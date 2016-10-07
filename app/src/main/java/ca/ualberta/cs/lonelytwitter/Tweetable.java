526b5c576358e049a465c98e8b8fc0cfacddd3ae
pshn111 <spei@ualberta.ca>
2016-10-07 08:45:20 -0600

    Update Tweetable.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -5,9 +5,24 @@ import java.util.Date;
 
 /**
  * Created by watts1 on 9/15/16.
+ * Modified by dmhamilt on 10/06/16.
+ *
+ * An unused interface version of Tweet
  */
 public interface Tweetable {
+    
+    /**
+     * Gets message.
+     *
+     * @return the message
+     */
     public String getMessage();
+    
+    /**
+     * Gets date.
+     *
+     * @return the date
+     */
     public Date getDate();
 
 
