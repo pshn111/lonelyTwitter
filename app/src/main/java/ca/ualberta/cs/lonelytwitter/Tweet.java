c17f635a112bc2b8e9604dfb0b828ee5cbc0bb68
pshn111 <spei@ualberta.ca>
2016-10-07 08:43:31 -0600

    Update Tweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -5,31 +5,63 @@ import java.util.Date;
 
 /**
  * Created by watts1 on 9/13/16.
+ * Modified by dmhamilt on 10/06/16.
+ * Tweet is an abstract class intended to hold a message.
+ *
+ * @see NormalTweet
+ * @see ImportantTweet
  */
 public abstract class Tweet  implements Tweetable, Comparable<Tweet> {
     protected String message;
     private Date date;
 
 
+    /**
+     * Sort Tweets.
+     *
+     * @param Tweet o1
+     */
     public int compareTo(Tweet o1) {
         return this.getDate().compareTo(o1.getDate());
     }
 
+    /**
+     * Instantiates a new Tweet.
+     *
+     * @param message the message
+     */
     public Tweet(String message){
         this.message = message;
         this.date = new Date();
     }
 
+    /**
+     * Instantiates a new Tweet.
+     *
+     * @param message the message
+     * @param date    the date
+     */
     public Tweet(String message, Date date){
         this.message = message;
         this.date = date;
     }
 
 
+    /**
+     * Is important boolean.
+     *
+     * @return the boolean
+     */
     public abstract Boolean isImportant();
 
 
 
+    /**
+     * Sets message.
+     *
+     * @param message the message
+     * @throws TweetTooLongException the tweet too long exception
+     */
 
     public void setMessage(String message) throws TweetTooLongException {
         if (message.length() > 140) {
@@ -39,6 +71,11 @@ public abstract class Tweet  implements Tweetable, Comparable<Tweet> {
         this.message = message;
     }
 
+    /**
+     * Sets date.
+     *
+     * @param date the date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
