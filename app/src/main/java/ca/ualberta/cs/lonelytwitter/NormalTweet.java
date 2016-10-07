8695c27b65189c656ec1cb8eca5d9b2aa7acad2e
pshn111 <spei@ualberta.ca>
2016-10-07 08:36:27 -0600

    Update NormalTweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -8,6 +8,11 @@ package ca.ualberta.cs.lonelytwitter;
  */
 public class NormalTweet extends Tweet implements Tweetable {
 
+    /**
+    * Instantiates a new Normal tweet.
+    *
+    * @param message the message
+    */
     public NormalTweet(String message) {
         super(message);
     }
